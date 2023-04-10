f39ffc04b9a4dbe6e5eaf22460d332bb2d759fac
Jamie Schiff <jamie.abrams.schiff@gmail.com>
2023-04-10 10:28:19 -0400

    adding prep algos

---
+++ interview-prep/bubble-prep.js
@@ -0,0 +1,70 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. 
+The remaining elements of nums are not important as well as the size of nums. Return k.
+*/
+
+//APPROACH = TWO POINTER
+//one pointer to iterate through the array, the other to keep track of the position where the next unique element should be
+//if the first el = second el
+//increment second pointer till we find a unique element
+//set the unique element to the position tracked by the other pointer
+//increment both pointers -> unique pointer is at next open spot, iterating pointer is at the next element to iterate over
+
+const removeDuplicates = (array) => {
+  let uniqueTracker = 0;
+  let iteratingPointer = 0;
+  //iterate over the array until iteratingPointer reaches the end
+  while (iteratingPointer < array.length) {
+    //if the elements are the same, incremenet the iteratingPointer
+    if (array[uniqueTracker] === array[iteratingPointer]) {
+      iteratingPointer++;
+      //if there is a unique vale
+    } else {
+      //set the next element in the array to the unique element
+      array[uniqueTracker + 1] = array[iteratingPointer];
+      //incremenet both pointers
+      uniqueTracker++;
+      iteratingPointer++;
+    }
+  }
+  //return the number of unique elements
+  return i + 1;
+};
+
+/*
+  Receives a variable number of arrays, and returns an array that contains every item shared between all passed-in arrays
+*/
+
+//APPROACH: loop through each item in the first array, check if it exists in all other arrays, and push it to the result array if it does.
+const findCommonItems = (...arrays) => {
+  const result = [];
+
+  if (arrays.length === 0) return result;
+
+  const firstArray = arrays[0];
+
+  for (let i = 0; i < firstArray.length; i++) {
+    const currentItem = firstArray[i];
+
+    if (result.includes(currentItem)) {
+      continue;
+    }
+    let itemFound = true;
+
+    for (let j = 1; j < arrays.length; j++) {
+      if (!arrays[j].includes(currentItem)) {
+        itemFound = false;
+        break;
+      }
+    }
+    if (itemFound) {
+      result.push(currentItem);
+    }
+  }
+  return result;
+};
